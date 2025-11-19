2153616d056a68ed293b0d211785edf30abb5a8a
Ankit kumar Singh <ankit12030621028@gmail.com>
2025-11-19 19:44:54 +0530

    3rd commit

--- main.js
+++ main.js
@@ -12,12 +12,26 @@ const playerD = Player.create();
 
 game1.addPlayer(playerA);
 game1.addPlayer(playerB);
-
 game2.addPlayer(playerC);
 game2.addPlayer(playerD);
 
 // game1.start();
 // game2.start();
 
+
+console.log("Before Move:");
+game1.render();
+game2.render();
+
+for(const p of game1.players){
+    p.moveTowards(game1.destination.x , game1.destination.y , game1.cols,game1.rows);
+}
+
+
+for(const p of game2.players){
+    p.moveTowards(game2.destination.x , game2.destination.y , game2.cols,game2.rows);
+}
+
+console.log("\nAfter one move:");
 game1.render();
 game2.render();
--- player.js
+++ player.js
@@ -17,4 +17,20 @@ export default class Player {
     this.x = x;
     this.y = y;
   }
+
+  moveTowards(targetX, targetY, gridWidth, gridHeight){
+    if(!this.isAlive)return;
+
+    const stepX = targetX > this.x ? 1 : targetX < this.x ? -1 : 0;
+    const stepY = targetY > this.y ? 1 : targetY < this.y ? -1 : 0;
+    
+    let nextX = this.x + stepX;
+    let nextY = this.y + stepY;
+
+    this.x = Math.min(Math.max(nextX,0),gridHeight-1);
+    this.y = Math.min(Math.max(nextY,0),gridWidth-1);
+
+  }
+
+
 } 
